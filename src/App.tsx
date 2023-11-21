a23b1efb68420e9262573612f5a972f54d7cde22
Tommy Lidkovsky <armkudr@gmail.com>
2023-11-21 18:37:43 +0200

    route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Main from "./pages/main/Main";
 import Create from "./pages/create/Create";
 
@@ -7,6 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<Navigate to={"/create"} />} />
         <Route path="main" element={<Main />} />
         <Route path="create" element={<Create />} />
       </Routes>
